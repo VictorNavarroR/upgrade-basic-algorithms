5598dfab9b13c00dd1cf200be1c747ba98705fa6
Victor Navarro <victor.navarro@bootcamp-upgrade.com>
2021-12-20 18:18:39 +0100

    Iteration#2

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 // Iretation 1
-const myFavoriteHero = "Hulk";
-const x = 50;
-const h = 5;
-const y = 10;
+// const myFavoriteHero = "Hulk";
+// const x = 50;
+// const h = 5;
+// const y = 10;
 
-const z = h + y;
+// const z = h + y;
 
+// Iretation 2
+//1.1
+const character = {name: 'Jack Sparrow', age: 10};
+character.age = 25;
 
+//1.2
+const firstName = "Jon";
+const lastName = "Snow";
+const age = 24;
+
+console.log(`Soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos.`);
+
+//1.3
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+
+console.log(toy1.price + toy2.price);
+
+//1.4
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+globalBasePrice = 25000;
+
+//update final price of car1 and car2
+car1.finalPrice = globalBasePrice + car1.basePrice;
+car2.finalPrice = globalBasePrice + car2.basePrice;
