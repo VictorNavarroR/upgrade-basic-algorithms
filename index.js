003bab1d57f3b0a9e7796bceca38701a59c7b09c
Victor Navarro <victor.navarro@bootcamp-upgrade.com>
2021-12-20 18:51:20 +0100

    Iteration#5

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Iretation 1
+// Iteration 1
 // const myFavoriteHero = "Hulk";
 // const x = 50;
 // const h = 5;
@@ -6,7 +6,7 @@
 
 // const z = h + y;
 
-// Iretation 2
+// Iteration 2
 //1.1
 const character = {name: 'Jack Sparrow', age: 10};
 character.age = 25;
@@ -35,7 +35,7 @@ globalBasePrice = 25000;
 car1.finalPrice = globalBasePrice + car1.basePrice;
 car2.finalPrice = globalBasePrice + car2.basePrice;
 
-// Iretation 3
+// Iteration 3
 //1.1
 alert(10*5);
 
@@ -53,7 +53,7 @@ let x = y + z;
 //1.5
 x = y * z;
 
-// Iretation 4
+// Iteration 4
 
 //1.1
 const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
@@ -80,3 +80,35 @@ rickAndMortyCharacters.pop();
 //1.6
 rickAndMortyCharacters.splice(1,1);
 
+// Iteration 5
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+if(number1 === 10){
+    console.log('number1 es estrictamente igual a 10')
+}
+
+if (number1 / number2) {
+  console.log("number2 dividido entre number1 es igual a 2");
+}
+
+if (number1 !== number2) {
+  console.log("number1 es estrictamente distinto a number2");
+}
+
+if (number3 != number1) {
+  console.log("number3 es distinto number1");
+}
+
+if (number3 * 5 === number1) {
+  console.log("number3 por 5 es igual a number1");
+}
+
+if (number3 * 5 === number1 && number1 * 2 === number2) {
+  console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+}
+
+if (number2 / 2  === number1 || number1 / 5 === number3) {
+  console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+}
